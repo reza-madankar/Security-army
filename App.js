0087c07bfb8ca7325ebe86a1db2a8b6218c7f5ab
Hooman <patriot_sh@yahoo.com>
2023-01-30 19:05:04 +0330

    Page home and Posts is coplete and create the comments page

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screen/Home";
 import Posts from "./screen/Posts";
+import Comments from "./screen/Comments";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -13,8 +14,8 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Posts" component={Posts} />
+        <Stack.Screen name="Comments" component={Comments} />
       </Stack.Navigator>
-  
     </NavigationContainer>
   );
 }
--- screen/Posts.js
+++ screen/Posts.js
@@ -1,10 +1,188 @@
-import { Button, StyleSheet, Text, View, CheckBox } from "react-native";
-import React from "react";
+import Checkbox from "expo-checkbox";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-const Posts = () => {
-  const [isSelected, setSelection] = useState(false);
+export default function Posts({ navigation }) {
+  const [isChecked, setChecked] = useState(false);
+  const [isChecke, setChecke] = useState(false);
 
-  return <View></View>;
-};
+  return (
+    <View style={styles.container}>
+      <Text style={styles.mafia}>Mafia</Text>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <Text style={styles.mafia}>Citizen</Text>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <View style={styles.section}>
+        <Checkbox
+          style={styles.checkbox}
+          value={isChecked}
+          onValueChange={setChecked}
+        />
+        <Text style={styles.paragraph}>Normal checkbox</Text>
+      </View>
+      <Button
+        title="Next"
+        onPress={() => {
+          navigation.navigate("Comments");
+        }}
+      ></Button>
+    </View>
+  );
+}
 
-export default Posts;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginVertical: 32,
+  },
+  section: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  paragraph: {
+    flex: 1,
+    fontSize: 25,
+  },
+  checkbox: {
+    margin: 8,
+  },
+  mafia: {
+    fontSize: 35,
+    textAlign: "center",
+  },
+});
