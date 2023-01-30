8c38e2254bfcf6dfec06fde6d24c53adbf5f3a4c
Hooman <patriot_sh@yahoo.com>
2023-01-30 19:20:23 +0330

    Loading player in page 3

--- screen/Comments.js
+++ screen/Comments.js
@@ -1,14 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+import data from "../data";
+data;
 const Comments = () => {
   return (
     <View>
-      <Text>Comments</Text>
+      <Text style={styles.player1}>
+        {data.player.map((item) => (
+          <Text style={styles.player}>
+            {item.name}
+            {"   "}
+          </Text>
+        ))}
+      </Text>
     </View>
   );
 };
 
 export default Comments;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  player: {
+    fontSize: 40,
+    borderColor: "black",
+    borderWidth: 2,
+    width: 20,
+    height: 40,
+  },
+  player1: {},
+});
